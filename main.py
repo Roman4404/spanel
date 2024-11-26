6b4a23e631c51612f75084fe7a585f8b702a06fa
Roman Lenshin <77573438+Roman4404@users.noreply.github.com>
2024-11-26 16:25:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ class Interface(QMainWindow): #Интерфейс
 
 
     def start_program_create_files(self):
-        subprocess.run(['./Initial_Setup_Windows/Initial_setup_main.exe'])
+        subprocess.run(['./pyt/Scripts/python.exe','./Initial_Setup_Windows/Initial_setup_main.py'])
 
     def settings_profile(self):
         et = SettingsProfile()
