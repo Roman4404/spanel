cddfebe1013f992c281753a24421157c54154e63
R4404 <r.v.lenshin@gmail.com>
2025-12-24 13:37:50 +0700

    Новые иконки Добавлены icon_v2

--- main.py
+++ main.py
@@ -825,7 +825,7 @@ class AI_Studio:
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon('./mainWindows/Interface/image/icon.png'))
+    app.setWindowIcon(QIcon('./mainWindows/Interface/image/icon_v2.png'))
     ex = Interface()
     ex.show()
     sys.exit(app.exec())
